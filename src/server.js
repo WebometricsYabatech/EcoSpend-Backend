b772000967f47e34c4283d2145a23bac615e574e
Adewale SHOGADE <toluwalopemi.shogade@gmail.com>
2026-06-23 15:23:31 +0100

    fix: fixed the CORS preflight errors

--- src/server.js
+++ src/server.js
@@ -1,25 +1,4 @@
-import expenseRoutes from './routes/expenses.js'
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import authRoutes from './routes/auth.js'
-import aiRoutes from './routes/ai.js'
-
-dotenv.config()
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/auth', authRoutes)
-app.use('/api/expenses', expenseRoutes)
-app.use('/api/ai', aiRoutes)
-app.use('/uploads', express.static('uploads'))
-
-app.get('/', (req, res) => {
-  res.send('Spendwise API is running')
-})
+import app from './app.js'
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
