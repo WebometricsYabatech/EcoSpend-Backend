27afc6aedb2258423599baab363bb7d07b84e7b2
Victory <victoryorilola@gmail.com>
2026-06-25 11:56:49 +0100

    An update

--- src/server.js
+++ src/server.js
@@ -1,4 +1,12 @@
 import app from './app.js'
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) 
+
+process.on("uncaughtException", (err) => {
+    console.error(err);
+});
+
+process.on("unhandledRejection", (err) => {
+    console.error(err);
+});
